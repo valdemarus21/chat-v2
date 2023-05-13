e6f61b95540fe878e50df8c4861d93daf90fde44
vladimir harmash <valdemarusfordev@protonmail.com>
2023-05-13 14:56:33 +0300

    add jsx layout to Main func. component

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-
+import React from 'react';
+import styles from '../styles/Main.module.css';
+import { Link } from 'react-router-dom';
 export const Main = () => {
-return (
-<>
-
-</>
-)
-}
+	return (
+		<div className={styles.wrap}>
+			<div className={styles.container}>
+				<h1 className={styles.heading}>Join</h1>
+				<form className={styles.form}>
+					<div className={styles.group}>
+						<input
+							type="text"
+							name="username"
+							required
+							className={styles.input}
+							onChange={() => {}}
+							placeholder="username..."
+						/>
+					</div>
+					<div className={styles.group}>
+						<input
+							type="text"
+							name="room"
+							required
+							className={styles.input}
+							onChange={() => {}}
+							placeholder="room"
+						/>
+					</div>
+					<Link to={`/chat?name=`}>
+						<button
+							type="submit"
+							className={styles.button}
+							style={{ padding: 15, borderRadius: 15 }}>
+							SIGN IN
+						</button>
+					</Link>
+				</form>
+			</div>
+		</div>
+	);
+};
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import './styles/main.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
