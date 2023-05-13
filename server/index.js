120fc4fb99ef730391294c5e0fa353c911f6c988
vladimir harmash <valdemarusfordev@protonmail.com>
2023-05-13 14:00:41 +0300

    create simple routing via express.router and create client part with react

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+const app = express();
+
+const server = http.createServer(app);
+const route = require('./route');
+app.use(cors({ origin: '*' }));
+app.use(route);
+
+const io = new Server(server, {
+	cors: {
+		origin: '*',
+		methods: ["GET", "POST"]
+	}
+})
+
+server.listen(5000, () => {
+	console.log('server is running');
+});
